66ad210d2969f1571124bd121e24ae4eb885bde9
jordanpindoh <110136631+jordanpindoh@users.noreply.github.com>
2025-09-12 21:01:02 +0100

    Add files via upload
    # --- Configuration de la page ---
    st.set_page_config(
        page_title="Prédiction de Churn Client",
        page_title="Prédiction du Churn Client Ecommerce SDUDA25",
    page_icon="🔮",
    layout="wide"
    )
    
    # --- Interface principale ---
    st.title("Prédiction du Churn Client 🔮")
    st.title("Prédiction du Churn Client Ecommerce SDUDA25 🔮")
    st.write("""
    Cette application prédit la probabilité qu'un client résilie son abonnement (churn).
    Cette application de Alexandre, Jordan pindoh, Ryad  prédit la probabilité qu'un client résilie son abonnement (churn).
    Ajustez les caractéristiques du client ci-dessous et cliquez sur "Lancer la Prédiction" pour voir le résultat.
    """)
    st.markdown("---")

---
+++ appp.py
@@ -0,0 +1,18 @@
+
+# --- Configuration de la page ---
+st.set_page_config(
+    page_title="Prédiction de Churn Client",
+    page_title="Prédiction du Churn Client Ecommerce SDUDA25",
+page_icon="🔮",
+layout="wide"
+)
+
+# --- Interface principale ---
+st.title("Prédiction du Churn Client 🔮")
+st.title("Prédiction du Churn Client Ecommerce SDUDA25 🔮")
+st.write("""
+Cette application prédit la probabilité qu'un client résilie son abonnement (churn).
+Cette application de Alexandre, Jordan pindoh, Ryad  prédit la probabilité qu'un client résilie son abonnement (churn).
+Ajustez les caractéristiques du client ci-dessous et cliquez sur "Lancer la Prédiction" pour voir le résultat.
+""")
+st.markdown("---")
